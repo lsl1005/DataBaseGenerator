8d4cefcb224472789d8e5dd9f238232a1ded712a
lsl1005 <lsl1005@alu.ubu.es>
2024-04-07 18:13:05 +0100

    Sustituir https en lugar de http en pom.xml para evitar el error en <project>

--- formacion/src/main/java/es/databasegenerator/formacion/App.java
+++ formacion/src/main/java/es/databasegenerator/formacion/App.java
@@ -13,28 +13,28 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.setTitle("DBG");
-        stage.show();
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
+		scene = new Scene(loadFXML("primary"), 640, 480);
+		stage.setScene(scene);
+		stage.setTitle("DBG");
+		stage.show();
+	}
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml)); //escena principal
-    }
+	static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml)); // escena principal
+	}
 
-    private static Parent loadFXML(String fxml) throws IOException {
-    	//carga la instancia recibida (primary.fxml)
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+	private static Parent loadFXML(String fxml) throws IOException {
+		// carga la instancia recibida (primary.fxml)
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
 
-    public static void main(String[] args) {
-        launch(); //lanza start
-    }
+	public static void main(String[] args) {
+		launch(); // lanza start
+	}
 
 }
